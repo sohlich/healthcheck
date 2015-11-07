b5afea5fbc90cf01f01fc13e02676ebb7a8a6d42
Radomir Sohlich <sohlich@gmail.com>
2015-11-07 20:59:56 +0100

    godoc

--- healthcheck.go
+++ healthcheck.go
@@ -1,3 +1,6 @@
+//Package healthcheck provides simple implementation of application
+//periodic health checker. That periodically checks the health of application
+//compoentns.
 package healthcheck
 
 import (
@@ -6,28 +9,68 @@ import (
 	"time"
 )
 
+//Errors that are thrown if HealthChecker is in illegal state.
 var (
-	ErrHealthcheckerNotStarted     = errors.New("HealthChecker not started yet.")
+	//ErrHealthcheckerNotStarted is returned by HealthChecker.Stop() method if
+	//the checker has not been started yet.
+	ErrHealthcheckerNotStarted = errors.New("HealthChecker not started yet.")
+	//ErrHealthcheckerAlreadyStarted is returned by HealthChecker.Start() method
+	//if the checker has been already started.
 	ErrHealthcheckerAlreadyStarted = errors.New("HealthChecker already started.")
 )
 
+//HealthIndicator is the method the wraps the function that check the
+//given application component.
+//
+//Health indicator should not been long running proccess, because the
+// indicators method IsHealthy is called in infinite loop of HelathChecker
+//and that could block othe health indicators to be callled for a long time.
 type HealthIndicator interface {
+
+	//Name should return
+	//the name of indicator.
 	Name() string
+
+	//IsHealthy return true if compoent
+	//is healthy and false in oposit case.
 	IsHealthy() bool
 }
 
+//HealthCheckerHook is a function that is called after
+//all health checks are done.
 type HealthCheckerHook func(map[string]bool)
 
+//HealthChecker interface wraps the functionality
+//of package. After the HealthChecker is started the infinite loop is
+//started and ii iterates over all regitered HealthIndicators. After the health check
+//is done, the result is then submitted to added hooks.
+//The HealthChecker  is considered to be thread safe.
 type HealthChecker interface {
+	//Start is called to start the infinite loop
+	//to check the HealthIndicators.
 	Start() error
+
+	//Stop stops the HealthChjeckers
+	//infinite loop.
 	Stop() error
+
+	//RegisterIndicator register the indicator
+	//to HelathChecker object.
 	RegisterIndicator(indicator HealthIndicator)
+
+	//UnregisterIndicator removes the HealthIndicator
+	//from the collection of checked inidcators.
 	UnregisterIndicator(indicator HealthIndicator)
+
+	//AddHook adds a hook to the collection of hooks to be called
+	//after all checks are done.
 	AddHook(name string, hook HealthCheckerHook)
+
+	//RemoveHook removes the hook by given name.
 	RemoveHook(name string)
 }
 
-type AsyncHealthChecker struct {
+type asyncHealthChecker struct {
 	period        time.Duration
 	indicatorLock *sync.Mutex
 	hookLock      *sync.Mutex
@@ -36,8 +79,9 @@ type AsyncHealthChecker struct {
 	stopChan      chan struct{}
 }
 
+//New creates new HealthChecker object.
 func New(period time.Duration) HealthChecker {
-	checker := &AsyncHealthChecker{
+	checker := &asyncHealthChecker{
 		period:        period,
 		indicatorLock: &sync.Mutex{},
 		hookLock:      &sync.Mutex{},
@@ -48,13 +92,13 @@ func New(period time.Duration) HealthChecker {
 	return checker
 }
 
-func (checker *AsyncHealthChecker) Start() error {
+func (checker *asyncHealthChecker) Start() error {
 	if checker.stopChan != nil {
 		return ErrHealthcheckerAlreadyStarted
 	}
 
 	checker.stopChan = make(chan struct{}, 0)
-	go func(ch *AsyncHealthChecker) {
+	go func(ch *asyncHealthChecker) {
 		for {
 			time.Sleep(ch.period)
 			select {
@@ -71,7 +115,9 @@ func (checker *AsyncHealthChecker) Start() error {
 
 				ch.hookLock.Lock()
 				for _, hook := range ch.hooks {
-					hook(res)
+					go func(r map[string]bool) {
+						hook(r)
+					}(res)
 				}
 				ch.hookLock.Unlock()
 			}
@@ -80,7 +126,7 @@ func (checker *AsyncHealthChecker) Start() error {
 	return nil
 }
 
-func (checker *AsyncHealthChecker) Stop() error {
+func (checker *asyncHealthChecker) Stop() error {
 	if checker.stopChan == nil {
 		return ErrHealthcheckerNotStarted
 	}
@@ -88,25 +134,25 @@ func (checker *AsyncHealthChecker) Stop() error {
 	return nil
 }
 
-func (checker *AsyncHealthChecker) RegisterIndicator(indicator HealthIndicator) {
+func (checker *asyncHealthChecker) RegisterIndicator(indicator HealthIndicator) {
 	checker.indicatorLock.Lock()
 	checker.indicators[indicator.Name()] = indicator
 	checker.indicatorLock.Unlock()
 }
 
-func (checker *AsyncHealthChecker) UnregisterIndicator(indicator HealthIndicator) {
+func (checker *asyncHealthChecker) UnregisterIndicator(indicator HealthIndicator) {
 	checker.indicatorLock.Lock()
 	delete(checker.indicators, indicator.Name())
 	checker.indicatorLock.Unlock()
 }
 
-func (checker *AsyncHealthChecker) AddHook(name string, hook HealthCheckerHook) {
+func (checker *asyncHealthChecker) AddHook(name string, hook HealthCheckerHook) {
 	checker.hookLock.Lock()
 	checker.hooks[name] = hook
 	checker.hookLock.Unlock()
 }
 
-func (checker *AsyncHealthChecker) RemoveHook(name string) {
+func (checker *asyncHealthChecker) RemoveHook(name string) {
 	checker.hookLock.Lock()
 	delete(checker.hooks, name)
 	checker.hookLock.Unlock()
